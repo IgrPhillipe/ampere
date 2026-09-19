7738982e95a9aa59c1953994da99f9a124fab7e6
KellwenIgor <kilc@cesar.school>
2026-09-19 02:57:13 -0300

    fix(front): prevent table header hover background

--- app/front/src/components/ui/table.tsx
+++ app/front/src/components/ui/table.tsx
@@ -23,7 +23,10 @@ function TableHeader({ className, ...props }: React.ComponentProps<"thead">) {
 	return (
 		<thead
 			data-slot="table-header"
-			className={cn("[&_tr]:border-b-2 [&_tr]:border-secondary", className)}
+			className={cn(
+				"[&_tr]:border-b-2 [&_tr]:border-secondary [&_tr]:hover:bg-transparent",
+				className,
+			)}
 			{...props}
 		/>
 	);
